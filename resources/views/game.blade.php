cbe71986c6a395e16b1f04c09cf35fc412ac1711
rodriguembah13 <rodriguembah13@gmail.com>
2024-09-02 11:34:09 +0100

    persit

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -59,7 +59,7 @@
                                             <div class="col-md-4 col-4">
                                                 <div class="mart__point__items">
                                                     <a href="javascript:void(0);" class="point__box" data-bs-toggle="tooltip" data-bs-placement="top" title="Nombres de jeux {{$occurences['v1']}}%">
-                                                        <input class="check-box" type="radio" name="{{$fixture->fixture_id}}" value="1" id="check1{{$item->id}}">
+                                                        <input @if($is_then==1) disabled @endif class="check-box" type="radio" name="{{$fixture->fixture_id}}" value="1" id="check1{{$item->id}}">
                                                         <label for="check1{{$item->id}}">
                                                             <span class="break">1({{$occurences['v1']}}%)</span>
                                                             <div><i class="fas fa-check">1</i></div>
@@ -67,7 +67,7 @@
 
                                                     </a>
                                                     <a href="javascript:void(0);" class="point__box" data-bs-toggle="tooltip" data-bs-placement="top" title="Nombres de jeux {{$occurences['v3']}}%">
-                                                        <input type="radio" name="{{$fixture->fixture_id}}" value="3" id="check3{{$item->id}}">
+                                                        <input @if($is_then==1) disabled @endif type="radio" name="{{$fixture->fixture_id}}" value="3" id="check3{{$item->id}}">
                                                         <label for="check3{{$item->id}}">
                                                             <span class="break">x({{$occurences['v3']}}%)</span>
                                                             <div><i class="fas fa-check"></i></div>
@@ -75,7 +75,7 @@
 
                                                     </a>
                                                     <a href="javascript:void(0);" class="point__box" data-bs-toggle="tooltip" data-bs-placement="top" title="Nombres de jeux {{$occurences['v2']}}%">
-                                                        <input type="radio" name="{{$fixture->fixture_id}}" value="2" id="check2{{$item->id}}">
+                                                        <input @if($is_then==1) disabled @endif type="radio" name="{{$fixture->fixture_id}}" value="2" id="check2{{$item->id}}">
                                                         <label for="check2{{$item->id}}">
                                                             <span class="break">2({{$occurences['v2']}}%)</span>
                                                             <div><i class="fas fa-check"></i></div>
